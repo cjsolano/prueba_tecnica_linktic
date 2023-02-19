dc2e03e7a9d8043b6952f49ddef66a9da5e408be
Julian S. Cruz <julianscruz@gmail.com>
2023-02-18 23:31:04 -0500

    report test

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Frontend Challenge
 
-Este es el test de Frontend Developer con Vue.js. Tiene un tiempo de hasta 1 día para entregar el test resuelto. Por favor, tome en cuenta el tiempo que se demora en resolver el test ya que es probable se le pregunte. Al completarlo, debe subir el código a un repositorio público de GitHub y compartirlo con la persona que le envió el test. 
+Dev: Cesar Julian Solano Cruz <julianscruz@gmail.com>
 
 ## Info
-
-El repositorio contiene una estructura inicial de un proyecto Vite.js. Para iniciar el ambiente de desarrollo en local solo debe correr el siguiente comando:
 
 ```
 npm run dev
@@ -17,24 +15,6 @@ Este comando inicializará el API de frutas de manera local y el endpoint que de
 ```
 http://127.0.0.1:8888/api/fruits
 ```
-
-## Requerimientos
- - Debe consultar el API de frutas para traerse los datos a mostrar en pantalla
- - Debe construir el componente desde cero (no use librerias de componentes)
- - Debe coincidir con el estilo del diseño de Figma 
- - Debe ser reusable, abierto para la extensión
- - Luego de completar el test, debe escribir un archivo REPORT.md donde debe argumentar las decisiones que tomó durante el test (ej: librerias implementadas, practicas de desarrollo, etc).
- - Opcional: se considerara como puntos a favor el uso de test unitarios o de integración
  
 ## Diseño
 Figma Link: https://www.figma.com/file/iRjstuUhyF17nHJ7sCk040/Dropdown-Test-Design?node-id=0%3A1 (debes iniciar sesion en Figma para inspeccionar el diseño)
-
-## Lo que estamos buscando:
- - Código mantenible y facil de testear
- - Separacion de responsabilidades
- - Uso de buenas practicas de desarrollo de software
- - Observar el uso de CSS y HTML moderno 
-
-## Tips
-
-Puede usar una libreria de CSS como TailwindCSS, Bulma o Bootstrap
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,36 +1,113 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import axios from "axios";
 
-import './Dropdown.css'
+import "./Dropdown.css";
 
-const baseURL = "http://127.0.0.1:8888/api";
+const Icon = () => {
+    return (
+        <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M12 15.5C11.8684 15.5008 11.7379 15.4755 11.6161 15.4258C11.4943 15.376 11.3835 15.3027 11.29 15.21L7.29 11.21C7.19676 11.1168 7.1228 11.0061 7.07234 10.8843C7.02188 10.7624 6.99591 10.6319 6.99591 10.5C6.99591 10.3681 7.02188 10.2376 7.07234 10.1158C7.1228 9.99393 7.19676 9.88324 7.29 9.79001C7.38324 9.69677 7.49393 9.62281 7.61575 9.57235C7.73758 9.52189 7.86814 9.49591 8 9.49591C8.13186 9.49591 8.26243 9.52189 8.38425 9.57235C8.50608 9.62281 8.61677 9.69677 8.71001 9.79001L12 13.1L15.3 9.92001C15.392 9.81772 15.5041 9.73551 15.6293 9.67852C15.7545 9.62154 15.8902 9.591 16.0277 9.58882C16.1653 9.58664 16.3018 9.61286 16.4288 9.66586C16.5557 9.71885 16.6704 9.79746 16.7656 9.89679C16.8607 9.99611 16.9344 10.114 16.9819 10.2431C17.0295 10.3722 17.0499 10.5097 17.0419 10.647C17.0338 10.7844 16.9976 10.9186 16.9353 11.0413C16.873 11.1639 16.7861 11.2724 16.68 11.36L12.68 15.22C12.4971 15.3963 12.254 15.4964 12 15.5Z"
+                fill="currentColor"
+            />
+        </svg>
+    );
+};
 
 function Dropdown() {
-    
     const [listFruits, setListFruits] = useState(null);
+    const [listSearch, setListSearch] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    const [dropdownActive, setDropdownActive] = useState(false);
 
     useEffect(() => {
-        axios.get("http://127.0.0.1:8888/api/fruits.json").then((response) => {
-            setListFruits(response.data.data.fruit);
-        });
-      }, []);
+        const getFruits = () => {
+            axios
+                .get("http://127.0.0.1:8888/api/fruits.json")
+                .then((response) => {
+                    setListFruits(response.data.data);
+                });
+        };
 
-      //console.log("listFruits",listFruits);
+        getFruits();
+    }, []);
 
-  return (
-    <div className="dropdownContainer">
-        <input
-            className="dropdownInput"
-            list="list-of-fruits"
-            placeholder="Select an item"
-        />
-        <datalist id="list-of-fruits">
-            {listFruits && listFruits.map((fruit, index) => (
-                <option key={index} value={fruit} />
-            ))}
-        </datalist>
-    </div>
-  )
+    function handleInputChange(event) {
+        const valorInput = event.target.value;
+        setInputValue(valorInput);
+
+        const search = listFruits.fruits.filter((item) =>
+            item.toLowerCase().includes(valorInput.toLowerCase())
+        );
+        setListSearch(search);
+    }
+
+    function handleClick() {
+        setDropdownActive(!dropdownActive);
+
+        if (!dropdownActive) {
+            setInputValue("");
+        } else if (listSearch.length == 0) {
+          setInputValue("");
+        }
+
+        const dropdown = document.querySelector(".dropdown");
+        const icon = document.querySelector(".icon");
+
+        dropdown.classList.toggle("active");
+        icon.classList.toggle("up");
+    }
+
+    function handleItemClick(event) {
+        handleClick();
+        setInputValue(event.target.innerText);
+    }
+
+    return (
+        <div className="dropdwnContainer">
+            <input
+                type="text"
+                placeholder={
+                    dropdownActive ? "This is a search input" : "Select an item"
+                }
+                list="list-of-fruits"
+                spellCheck="false"
+                readOnly={!dropdownActive}
+                value={inputValue}
+                onChange={handleInputChange}
+            />
+            <span className="icon" onClick={handleClick}>
+                <Icon />
+            </span>
+            <div className="dropdown">
+                <div className="list-of-fruits">
+                    {inputValue.length == 0 &&
+                        listFruits &&
+                        listFruits.fruits.map((fruit, index) => (
+                            <li key={index} onClick={handleItemClick}>
+                                {fruit}
+                            </li>
+                        ))}
+                    {inputValue.length > 0 && listSearch.length > 0
+                    ? (
+                      listSearch.map((fruit, index) => (
+                        <li key={index} onClick={handleItemClick}>
+                            {fruit}
+                        </li> 
+                    ))
+                    )
+                    : listSearch.length == 0 && <div className="disabled">No items were found.</div>
+                        }
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Dropdown
+export default Dropdown;
